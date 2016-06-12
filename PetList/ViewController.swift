f559b37c241af10a0071cc3edbb49f5fdc0ac5ea
Fauziah Permatasari <fauziahpermatasari@fauziahs-macbook-pro.local>
2016-06-12 21:09:57 +0800

    Initial Commit

---
+++ PetList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PetList
+//
+//  Created by Fauziah Permatasari on 12/06/2016.
+//  Copyright © 2016 Fauziah Permatasari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
